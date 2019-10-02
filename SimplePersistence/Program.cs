d67d731960d4c8ed58d91a06f118d23459c1c432
DEUSYEET <47902658+DEUSYEET@users.noreply.github.com>
2019-10-02 10:48:16 -0600

    Init

---
+++ SimplePersistence/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SimplePersistence
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
